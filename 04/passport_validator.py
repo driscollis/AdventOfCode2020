25f2173cef5ca07af35dd02ed4a4595c0a89f0f9
Mike Driscoll <mike@pythonlibrary.org>
2020-12-04 09:24:53 -0600

    add non-working day 4 answer

---
+++ 04/passport_validator.py
@@ -0,0 +1,102 @@
+data = """ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in"""
+
+def process(lines):
+    d = lines.split(' ')
+    dd = dict([item.split(':') for item in d if item])
+    keys = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+    for key in keys:
+        if key not in dd:
+            return False
+    return True
+
+
+
+    #byr (Birth Year) - four digits; at least 1920 and at most 2002.
+    #iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+    #eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+    #hgt (Height) - a number followed by either cm or in:
+        #If cm, the number must be at least 150 and at most 193.
+        #If in, the number must be at least 59 and at most 76.
+    #hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+    #ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+    #pid (Passport ID) - a nine-digit number, including leading zeroes.
+    #cid (Country ID) - ignored, missing or not.
+
+
+def process2(lines):
+    d = lines.split(' ')
+    dd = dict([item.split(':') for item in d if item])
+
+    keys = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+    for key in keys:
+        if key not in dd:
+            return False
+    if len(dd['byr']) != 4:
+        return False
+    if len(dd['iyr']) != 4:
+        return False
+    if len(dd['eyr']) != 4:
+        return False
+
+    if '1920' > dd['byr'] or dd['byr'] > '2002':
+        return False
+    if dd['iyr'] < "2010" or dd['iyr'] > "2020":
+        return False
+    if dd['eyr'] < "2020" or dd['eyr'] > "2030":
+        return False
+    if not any([dd['hgt'].find('in'), dd['hgt'].find('cm')]):
+        return False
+    if 'cm' in dd['hgt']:
+        height = int(dd['hgt'][:-2])
+        if height < 150 or height > 193:
+            return False
+    if 'in' in dd['hgt']:
+        height = int(dd['hgt'][:-2])
+        if height < 59 or height > 76:
+            return False
+    if '#' not in dd['hcl']:
+        return False
+    hcl = dd['hcl'].split('#')[-1]
+    if len(hcl) != 6:
+        return False
+    try:
+        int(hcl, 16)
+    except ValueError:
+        return False
+    eye_colors = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+    if dd['ecl'] not in eye_colors:
+        return False
+    if len(dd['pid']) != 9:
+        return False
+    return True
+
+
+def count_passports(data: str):
+    pport = {}
+    s = ''
+    count = 0
+    for line in data.split('\n'):
+        if line.strip() == '':
+            # process
+            count += bool(process2(s))
+            s = ''
+        s += ' ' + line
+    print(f"Valid passports: {count}")
+    return count
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        data = f.read()
+    count_passports(data)
